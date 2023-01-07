1202d3ed1bde4d00977639f518efd07fb8f732e2
MarceloVA <100235694+MarceloVA@users.noreply.github.com>
2023-01-07 13:58:09 -0600

    update

---
+++ lightmode.js
@@ -0,0 +1,7 @@
+let checkbox = document.querySelector("checkbox");
+
+
+if (checkbox.checked == true)
+    {
+        console.log('yes');
+    }
--- toggle.js
+++
@@ -1,3 +0,0 @@
-let checkbox = document.querySelector(".checkbox");
-
-console.log(checkbox.checked);
